5515054fddccfd9bbfaf5b713b9a97c4fab518b2
Tim Zabawa <timothyzabawa@gmail.com>
2022-12-19 22:02:30 -0500

    Add UI to root route, Replace semantic-ui css module import with temp link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import { Button, Card } from "semantic-ui-react";
+// import "semantic-ui-css/semantic.min.css";
 import factory from "../ethereum/factory";
-import "semantic-ui-css/semantic.min.css";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -8,8 +9,29 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => ({
+      description: <a>View Campaign</a>,
+      fluid: true,
+      header: address,
+    }));
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>;
+    return (
+      <div>
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
+        <h3>Open Campaigns</h3>
+        {this.renderCampaigns()}
+        <Button content="Create Campaign" icon="add circle" primary />
+      </div>
+    );
   }
 }
 
