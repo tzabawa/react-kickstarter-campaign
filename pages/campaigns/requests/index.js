589192af14ec76cb3b791c0c3eb0de9b845c33f9
Tim Zabawa <timothyzabawa@gmail.com>
2023-01-13 05:22:08 -0500

    Add 'RequestsIndex' component, Add '/campaigns/:address/requests' route, Add 'Link' tag to 'RequestsIndex' component

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import Layout from "../../../components/Layout";
+import { Link } from "../../../routes";
+
+class RequestsIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+
+    return {
+      address,
+    };
+  }
+  render() {
+    return (
+      <Layout>
+        <h3>Requests List</h3>;
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
+        </Link>
+      </Layout>
+    );
+  }
+}
+
+export default RequestsIndex;
--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const routes = require("next-routes")();
 
-routes.add("/campaigns/new", "/campaigns/new");
-routes.add("/campaigns/:address", "/campaigns/show");
+routes
+  .add("/campaigns/new", "/campaigns/new")
+  .add("/campaigns/:address", "/campaigns/show")
+  .add("/campaigns/:address/requests", "/campaigns/requests/index");
 
 module.exports = routes;
